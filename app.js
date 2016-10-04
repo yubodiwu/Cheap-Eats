6c3a228042aac9faf24260ef7df61e9acc44ca0c
Yubo Allan Diwu <yubo.diwu@gmail.com>
2016-10-04 09:10:57 -0700

    fixed bug in the app.js file

--- app.js
+++ app.js
@@ -33,6 +33,7 @@ class GrouponData {
         $.ajax({
             url: `http://api.groupon.com/v2/deals/?client_id=e4452424d8c2dcd047b191712adc6ba0167342f2&postal_code=${zipcode}`,
             method: 'GET',
+            dataType: 'jsonp',
             success: function(data) {
                 this.dealData.push(data);
             }

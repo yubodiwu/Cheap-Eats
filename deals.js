d0ceef3c31f2efb2ace246a1dfc03fae011ade28
Yubo Allan Diwu <yubo.diwu@gmail.com>
2016-10-04 17:44:23 -0700

    fixed some unidentified fields inthe parsed GroupOn object

--- deals.js
+++ deals.js
@@ -1,3 +1,12 @@
+'use strict';
+
 window.onload = function() {
-    console.log(test2);
+    var groupOnDataString = JSON.parse(localStorage.getItem('groupOnData'));
+    var groupOnData = []
+
+    for (let key in groupOnDataString) {
+        groupOnData.push(JSON.parse(groupOnDataString[key]));
+    }
+
+    console.log(groupOnData);
 }
